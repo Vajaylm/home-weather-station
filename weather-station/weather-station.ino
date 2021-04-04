aaa9e8a48a8491f75b68bd106fae8fc4efbcba69
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-04-04 13:49:23 +0200

    Implement data collection from DS1307 RTC

--- weather-station/weather-station.ino
+++ weather-station/weather-station.ino
@@ -1,42 +1,64 @@
+#include <RTClib.h>
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-
+RTC_DS1307 rtc;
 Adafruit_BME280 bme;
 
 void setup() {
   Serial.begin(9600);
-
+  
+  rtc.begin();
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    rtc.adjust(DateTime(__DATE__, __TIME__));
+  }
+  
   if (!bme.begin(0x76)) {
     Serial.println("Could not find BME280 sensor!");
     while (true);
   }
-  else {
-    Serial.println("BME280 sensor found!");
-    Serial.println("");
-  }
 }
 
 void loop() {
+  DateTime now = rtc.now();
+  DatePrint(now);
+  TimePrint(now);
+
   float temperature = bme.readTemperature();
   float pressure = bme.readPressure() / 100.0F;
   float humidity = bme.readHumidity();
-  float alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
-  FormattedPrint("Temperature", temperature, "C");
-  FormattedPrint("Pressure", pressure, "hPa");
-  FormattedPrint("Humidity", humidity, "%");
-  FormattedPrint("Altitude", alt, "m");
-
+  
+  FormattedDataPrint("Temperature", temperature, "C");
+  FormattedDataPrint("Pressure", pressure, "hPa");
+  FormattedDataPrint("Humidity", humidity, "%");
+  
   Serial.println("----------------------------");
   delay(2000);
 }
 
-void FormattedPrint(String prop, float value, String valUnit) {
+void FormattedDataPrint(String prop, float value, String valUnit) {
   Serial.print(prop);
   Serial.print(": ");
   Serial.print(value);
   Serial.println(valUnit);
+}
+
+void DatePrint(DateTime actDateTime) {
+  Serial.print(actDateTime.year(), DEC);
+  Serial.print('.');
+  Serial.print(actDateTime.month(), DEC);
+  Serial.print('.');
+  Serial.println(actDateTime.day(), DEC);
+}
+
+void TimePrint(DateTime actDateTime) {
+  Serial.print(actDateTime.hour(), DEC);
+  Serial.print(':');
+  Serial.print(actDateTime.minute(), DEC);
+  Serial.print(':');
+  Serial.print(actDateTime.second(), DEC);
+  Serial.println();
 }
