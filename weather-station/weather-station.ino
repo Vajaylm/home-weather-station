04e1e12776243d6ea94ada4dcf532a92a319f564
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-04-04 12:51:01 +0200

    Implement data collection from BME280

--- weather-station/weather-station.ino
+++ weather-station/weather-station.ino
@@ -2,6 +2,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 Adafruit_BME280 bme;
 
 void setup() {
@@ -13,11 +15,33 @@ void setup() {
   }
   else {
     Serial.println("BME280 sensor found!");
+    Serial.println("");
   }
-
 }
 
 void loop() {
+  float temperature = bme.readTemperature();
+  float pressure = bme.readPressure() / 100.0F;
+  float humidity = bme.readHumidity();
+  float alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
   
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println("C");
 
+  Serial.print("Pressure: ");
+  Serial.print(pressure);
+  Serial.println("hPa");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  Serial.print("Altitude: ");
+  Serial.print(alt);
+  Serial.println("m");
+
+
+  Serial.println("----------------------------");
+  delay(2000);
 }
