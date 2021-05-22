efd00ca6cfc52489e15aeba1fec0e449b36e09fa
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-05-22 10:23:00 +0200

    Restore data collection as with Arduino

--- weather-station/weather-station.ino
+++ weather-station/weather-station.ino
@@ -14,7 +14,7 @@ Adafruit_BME280 bme;
 
 int SAMPLING_TIME = 5000; //ms
 
-void setup() {  
+void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
   Wire.begin(D2, D1); // D1 - SCL, D2 - SDA
@@ -77,57 +77,49 @@ void setup() {
 }
 
 void loop() {
+  long startTime = millis();
+  
+  DateTime now = rtc.now();
+  DatePrint(now);
+  TimePrint(now);
+
+  float temperature = bme.readTemperature();
+  float pressure = bme.readPressure() / 100.0F;
+  float humidity = bme.readHumidity();
+  FormattedDataPrint("Temperature", temperature, "C");
+  FormattedDataPrint("Pressure", pressure, "hPa");
+  FormattedDataPrint("Humidity", humidity, "%");
+  Serial.println("----------------------------");
+  
+  delay(startTime + SAMPLING_TIME - millis());
+  
   ArduinoOTA.handle();
-  delay(500);
 }
 
+void FormattedDataPrint(String prop, float value, String valUnit) {
+  Serial.print(prop);
+  Serial.print(": ");
+  Serial.print(value);
+  Serial.println(valUnit);
+}
 
+void DatePrint(DateTime actDateTime) {
+  Serial.print(actDateTime.year(), DEC);
+  Serial.print('.');
+  Serial.print(TwoDigitFormatter(actDateTime.month()));
+  Serial.print('.');
+  Serial.println(TwoDigitFormatter(actDateTime.day()));
+}
 
+void TimePrint(DateTime actDateTime) {
+  Serial.print(TwoDigitFormatter(actDateTime.hour()));
+  Serial.print(':');
+  Serial.print(TwoDigitFormatter(actDateTime.minute()));
+  Serial.print(':');
+  Serial.print(TwoDigitFormatter(actDateTime.second()));  
+  Serial.println();
+}
 
-
-//
-//void loop() {
-//  long startTime = millis();
-//  
-//  DateTime now = rtc.now();
-//  DatePrint(now);
-//  TimePrint(now);
-//
-//  float temperature = bme.readTemperature();
-//  float pressure = bme.readPressure() / 100.0F;
-//  float humidity = bme.readHumidity();
-//  FormattedDataPrint("Temperature", temperature, "C");
-//  FormattedDataPrint("Pressure", pressure, "hPa");
-//  FormattedDataPrint("Humidity", humidity, "%");
-//  Serial.println("----------------------------");
-//  
-//  delay(startTime + SAMPLING_TIME - millis());
-//}
-//
-//void FormattedDataPrint(String prop, float value, String valUnit) {
-//  Serial.print(prop);
-//  Serial.print(": ");
-//  Serial.print(value);
-//  Serial.println(valUnit);
-//}
-//
-//void DatePrint(DateTime actDateTime) {
-//  Serial.print(actDateTime.year(), DEC);
-//  Serial.print('.');
-//  Serial.print(TwoDigitFormatter(actDateTime.month()));
-//  Serial.print('.');
-//  Serial.println(TwoDigitFormatter(actDateTime.day()));
-//}
-//
-//void TimePrint(DateTime actDateTime) {
-//  Serial.print(TwoDigitFormatter(actDateTime.hour()));
-//  Serial.print(':');
-//  Serial.print(TwoDigitFormatter(actDateTime.minute()));
-//  Serial.print(':');
-//  Serial.print(TwoDigitFormatter(actDateTime.second()));  
-//  Serial.println();
-//}
-//
-//String TwoDigitFormatter(int value) {
-//  return value < 10 ? "0" + String(value) : String(value);
-//}
+String TwoDigitFormatter(int value) {
+  return value < 10 ? "0" + String(value) : String(value);
+}
